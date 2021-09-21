e28f9986984e6a5d9d5d8a52c51cc8df664f5ec8
jkll556 <pspmlock87@gmail.com>
2021-09-21 10:02:03 +0900

    공통모듈분리

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () {
-  //logic.. search라는 클래스 클릭하면 focus
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function (){
-  searchEl.classList.add('focused'); //속성이름추가
-  searchInputEl.setAttribute('placeholder', '통합검색'); //속성추가
-});
-
-searchInputEl.addEventListener('blur', function (){
-  searchEl.classList.remove('focused'); //속성이름추가
-  searchInputEl.setAttribute('placeholder', ''); //속성추가
-});
-
-
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
@@ -165,6 +145,3 @@ spyEls.forEach(function (spyEl) {
 });
 
 
-//올해가 몇년도인지 자동ㅖ산
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); //2021
